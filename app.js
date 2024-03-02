8d0c94c1a41f37f5a5422beba7527dab4e4b6b5a
AkhiSharmin <akhisharmin100@gmail.com>
2024-03-02 21:20:18 +0600

    implement increment decrement

--- app.js
+++ app.js
@@ -10,22 +10,34 @@ let result = 0;
 
 function main() {
     const output = document.getElementById('output');
+    const incrementBtn = document.getElementById('increment-btn');
     const incrementInp = document.getElementById('increment-inp');
+
+    const decrementBtn = document.getElementById('decrement-btn');
     const decrementInp = document.getElementById('decrement-inp');
 
-    const incrementBtn = document.getElementById('increment-btn');
-    const decrementBtn = document.getElementById('decrement-btn');
-
     displayResult(output);
+
+    incrementBtn.addEventListener('click', function () {
+        const increment = parseInt(incrementInp.value);
+        result += increment;
+        displayResult(output);
+    })
+
+    decrementBtn.addEventListener('click', function () {
+        const decrement = parseInt(decrementInp.value);
+        result -= decrement;
+        displayResult(output);
+    });
 }
 
 function displayResult(output) {
-    let finalResult = null;
+    let finalResult = result;
     if (result < 10) {
         finalResult = `0${result}`
     }
 
-    output.innText = finalResult;
+    output.innerText = finalResult;
 }
 
 
